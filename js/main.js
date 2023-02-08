7b27bf596e068ecac3e29f67416651176f74e4e0
barium-bromide <108572893+barium-bromide@users.noreply.github.com>
2023-02-08 14:31:35 +0800

    tidy up

--- js/main.js
+++ js/main.js
@@ -3,8 +3,10 @@ const choice2 = document.getElementById("choice-2");
 
 choice1.onclick = () => {
     choice1.classList.add("clicked");
+    choice2.classList.remove("clicked");
 };
 
 choice2.onclick = () => {
     choice2.classList.add("clicked");
+    choice1.classList.remove("clicked");
 };
