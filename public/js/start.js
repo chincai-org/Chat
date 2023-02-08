0719a2cff4588d375f9eccff74a478e9ebfda735
barium-bromide <108572893+barium-bromide@users.noreply.github.com>
2023-02-08 16:40:57 +0800

    tidy up

--- public/js/start.js
+++ public/js/start.js
@@ -1,10 +1,1 @@
-<<<<<<< HEAD:js/start.js
 const login = document.getElementById("loginBtn");
-=======
-const homeLogin = document.getElementById("login");
-const login = document.getElementById("loginBtn");
-
-homeLogin.onclick = () => {
-    window.location.href = "login.html";
-};
->>>>>>> 6b7096e9f061898e939dcfad9238d1b22a1e3d42:public/js/start.js
