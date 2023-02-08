6f998b269603c4b507f9ffb5da4f46d8131a0035
ddddddddrrdd3 <108572893+ddddddddrrdd3@users.noreply.github.com>
2023-02-08 12:40:01 +0800

    topic half done

---
+++ main.js
@@ -0,0 +1,10 @@
+const choice1 = document.getElementById("choice-1");
+const choice2 = document.getElementById("choice-2");
+
+choice1.onclick = () => {
+    choice1.classList.add("clicked");
+};
+
+choice2.onclick = () => {
+    choice2.classList.add("clicked");
+};
