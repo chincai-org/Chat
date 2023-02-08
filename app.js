9c718699c76cbd4e8d48b97bf79b7d9c27e3b61d
Mortis66666 <mortisloverbs@gmail.com>
2023-02-08 15:46:29 +0800

    Kaboom

---
+++ app.js
@@ -0,0 +1,37 @@
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+const port = process.env.PORT || 3000;
+
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs");
+
+app.get("/chat", (req, res) => {
+    res.render("main.ejs");
+});
+
+app.get("/home", (req, res) => {
+    res.render("home.ejs");
+});
+
+app.get("/about", (req, res) => {
+    res.render("about.ejs");
+});
+
+app.get("/login", (req, res) => {
+    res.render("login.ejs");
+});
+app.get("/signup", (req, res) => {
+    res.render("signup.ejs");
+});
+
+server.listen(port, () => {
+    console.log(`Running server at http://localhost:${port}`);
+});
