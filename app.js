605c5bfca1d46117cbf5bd04ef1dbf0e96366e61
XLe Lee <xlelee.socialmedia@gmail.com>
2023-02-09 18:52:04 +0800

    Halfway done (signup user)?

--- app.js
+++ app.js
@@ -31,8 +31,36 @@ app.route("/chat")
     .post(async (req, res) => {
         let { username, password, cpassword } = req.body;
 
-        if (cpassword) {
-            await utils.createUser(username, password);
+        
+        if (username && password && cpassword && password == cpassword) {
+            //create a new token for cookie
+            let cookie = "";
+            const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            const charactersLength = characters.length;
+            let counter = 0;
+            while (counter < 20) {
+                cookie += characters.charAt(Math.floor(Math.random() * charactersLength));
+                counter += 1;
+            } 
+            //check if username or cookie exist before (return true if no duplication return false if got duplication)
+            if (await utils.checkExist(cookie, username)) {
+
+                //trying to create a user and check if the return is true (no problem) or false (got problem)
+                if (await utils.createUser(cookie, username, password)) {
+                    //user is created
+                    res.cookie("cookies", cookie)
+                    res.render("main.ejs")
+                    
+                }
+            } else {
+                //username or token existed, need a new one
+            }
+        } else {
+            //username is empty
+            //password is empty
+            //cpassword is empty
+            //password and cpassword is not the same
+
         }
 
         res.cookie("username", username);
