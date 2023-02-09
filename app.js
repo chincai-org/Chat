b4f6a6667d0e6c32e04a28ca9b650151bb9cce6e
Mortis66666 <mortisloverbs@gmail.com>
2023-02-09 17:10:24 +0800

    Login and signup template

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import http from "http";
 import { Server } from "socket.io";
+import cookieParser from "cookie-parser";
+import * as utils from "./utils.js";
 
 const app = express();
 const server = http.createServer(app);
@@ -10,12 +12,9 @@ const port = process.env.PORT || 3000;
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.set("view engine", "ejs");
-
-app.get("/chat", (req, res) => {
-    res.render("main.ejs");
-});
 
 app.get("/home", (req, res) => {
     res.render("home.ejs");
@@ -25,9 +24,25 @@ app.get("/about", (req, res) => {
     res.render("about.ejs");
 });
 
+app.route("/chat")
+    .get((req, res) => {
+        res.render("main.ejs");
+    })
+    .post((req, res) => {
+        let { username, password, cpassword } = req.body;
+
+        if (cpassword) {
+            utils.createUser(username, password);
+        }
+
+        res.cookie("username", username);
+        res.render("main.ejs");
+    });
+
 app.get("/login", (req, res) => {
     res.render("login.ejs");
 });
+
 app.get("/signup", (req, res) => {
     res.render("signup.ejs");
 });
