56845a1cbb22337154ccad5b2c292d80489ac199
Mortis66666 <mortisloverbs@gmail.com>
2023-02-09 17:10:24 +0800

    Login and signup template

---
+++ utils.js
@@ -0,0 +1,8 @@
+// import { MongoClient } from "mongodb";
+// import { config } from "dotenv";
+
+// config();
+// const uri = process.env.uri;
+// const client = new MongoClient(uri);
+
+export function createUser() {}
