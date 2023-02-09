de88ce8c5bf9752c4d26ce2da2b3b6c33265033e
XLe Lee <xlelee.socialmedia@gmail.com>
2023-02-09 22:02:26 +0800

    Signup function halfway done right??

--- app.js
+++ app.js
@@ -9,6 +9,18 @@ const server = http.createServer(app);
 const io = new Server(server);
 const port = process.env.PORT || 3000;
 
+function createCookie(l) {
+    let cookie = "";
+    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    const charactersLength = characters.length;
+    let counter = 0;
+    while (counter < l) {
+        cookie += characters.charAt(Math.floor(Math.random() * charactersLength));
+        counter += 1;
+    }
+    return cookie
+}
+
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -30,49 +42,57 @@ app.route("/chat")
     })
     .post(async (req, res) => {
         let { username, password, cpassword } = req.body;
+        //signup
+        if (cpassword) {
+                
+            if (username && password && cpassword && password == cpassword) {
+                //check if username or cookie exist before (return true if no duplication return false if got duplication)
+                let cookie = createCookie(20);
+                var checkExist = await utils.checkExist(cookie, username); //if cookie exists return 1, username exists return 1
+                var checkExist_num = 0
+                while (checkExist == 0) {
+                    if (checkExist_num < 10) {
+                        cookie = createCookie(20)
+                    } else {
+                        console.log("Your system got problem. ")
+                        exit()
+                    }
+                }            
+                if (checkExist == 1) {
+                    res.render("signup.ejs", {username: "<p> Username exists. </p>", password: ""});
+                } else {
 
-        
-        if (username && password && cpassword && password == cpassword) {
-            //create a new token for cookie
-            let cookie = "";
-            const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-            const charactersLength = characters.length;
-            let counter = 0;
-            while (counter < 20) {
-                cookie += characters.charAt(Math.floor(Math.random() * charactersLength));
-                counter += 1;
-            } 
-            //check if username or cookie exist before (return true if no duplication return false if got duplication)
-            if (await utils.checkExist(cookie, username)) {
+                    //trying to create a user and check if the return is true (no problem) or false (got problem)
+                    if (await utils.createUser(cookie, username, password)) {
+                        //user is created
 
-                //trying to create a user and check if the return is true (no problem) or false (got problem)
-                if (await utils.createUser(cookie, username, password)) {
-                    //user is created
-                    res.cookie("cookies", cookie)
-                    res.render("main.ejs")
-                    
+                        res.cookie("cookies", cookie)
+                        res.render("main.ejs")
+                        
+                    } else {
+                        
+                    }
                 }
             } else {
-                //username or token existed, need a new one
+                //username is empty
+                //password is empty
+                //cpassword is empty
+                //password and cpassword is not the same
+                res.render("signup.ejs")
+
             }
         } else {
-            //username is empty
-            //password is empty
-            //cpassword is empty
-            //password and cpassword is not the same
+            //login
 
         }
-
-        res.cookie("username", username);
-        res.render("main.ejs");
     });
 
 app.get("/login", (req, res) => {
-    res.render("login.ejs");
+    res.render("login.ejs", {username: "", password: ""});
 });
 
 app.get("/signup", (req, res) => {
-    res.render("signup.ejs");
+    res.render("signup.ejs", {username: "", password: ""});
 });
 
 server.listen(port, () => {
