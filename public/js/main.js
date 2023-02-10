d5d8fcb9eaedad1c66a8b0086567654685ebc3da
barium-bromide <108572893+barium-bromide@users.noreply.github.com>
2023-02-10 13:07:31 +0800

    tidy up and added some search bar and add topic

--- public/js/main.js
+++ public/js/main.js
@@ -4,21 +4,33 @@ const screenHeight = screen.height;
 
 choice1.onclick = () => {
     choice1.classList.add("clicked");
-    choice2.classList.remove("clicked");
+    if (choice2.classList.contains("clicked")) {
+        choice2.classList.remove("clicked");
+    }
 };
 
 choice2.onclick = () => {
     choice2.classList.add("clicked");
-    choice1.classList.remove("clicked");
+    if (choice1.classList.contains("clicked")) {
+        choice1.classList.remove("clicked");
+    }
 };
 
-$('textarea').each(function () {
-  this.setAttribute('style', 'height:' + (this.scrollHeight / window.innerHeight * 100) + 'vh;overflow-y:scroll;');
-}).on('input', function () {
-  this.style.height = 'auto';
-  if (this.scrollHeight > window.innerHeight / 7) { // Maximum height of 50% of the viewport height
-    this.style.height = '14.2vh';
-  } else {
-    this.style.height = (this.scrollHeight / window.innerHeight * 100) + 'vh';
-  }
-});
+$("#text")
+    .each(function () {
+        this.setAttribute(
+            "style",
+            "height:" +
+                (this.scrollHeight / window.innerHeight) * 100 +
+                "vh;overflow-y:scroll;"
+        );
+    })
+    .on("input", function () {
+        this.style.height = "auto";
+        if (this.scrollHeight > window.innerHeight / 6) {
+            this.style.height = "16.6vh";
+        } else {
+            this.style.height =
+                (this.scrollHeight / window.innerHeight) * 100 + "vh";
+        }
+    });
