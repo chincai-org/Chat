81340926348d0e81f16377541999201f29c889d5
ddddddddrrdd3 <108572893+ddddddddrrdd3@users.noreply.github.com>
2023-02-10 13:07:31 +0800

    tidy up and added some search bar and add topic

--- app.js
+++ app.js
@@ -11,14 +11,17 @@ const port = process.env.PORT || 3000;
 
 function createCookie(l) {
     let cookie = "";
-    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    const characters =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
     const charactersLength = characters.length;
     let counter = 0;
     while (counter < l) {
-        cookie += characters.charAt(Math.floor(Math.random() * charactersLength));
+        cookie += characters.charAt(
+            Math.floor(Math.random() * charactersLength)
+        );
         counter += 1;
     }
-    return cookie
+    return cookie;
 }
 
 app.use(express.static("public"));
@@ -42,7 +45,6 @@ app.route("/chat")
     })
     .post(async (req, res) => {
         let { username, password, cpassword } = req.body;
-        
     });
 app.post("/doingthings", (req, res) => {
     let { name, username, password, confirmpassword } = req.body;
@@ -53,81 +55,128 @@ app.post("/doingthings", (req, res) => {
                     if (!username.match[/[^A-z0-9]/g]) {
                         if (utils.UsernameExists(username)) {
                             if (password == confirmpassword) {
-                                let id = "id"
+                                let id = "id";
                                 while (id == "id") {
-                                    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+                                    const characters =
+                                        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
                                     const charactersLength = characters.length;
                                     let counter = 0;
                                     while (counter < 20) {
-                                        id = characters.charAt(Math.floor(Math.random() * charactersLength));
+                                        id = characters.charAt(
+                                            Math.floor(
+                                                Math.random() * charactersLength
+                                            )
+                                        );
                                         counter += 1;
-                                    } 
-                                    if (utils.IdExists(id)) {
-                                        id = id
-                                    } else {
-                                        id = "id"
                                     }
-                                  
+                                    if (utils.IdExists(id)) {
+                                        id = id;
+                                    } else {
+                                        id = "id";
+                                    }
                                 }
-                                if (utils.CreateAccount(username, password, id)) {
+                                if (
+                                    utils.CreateAccount(username, password, id)
+                                ) {
                                     res.cookie("id", id);
                                     res.redirect("/chat");
                                 }
                             } else {
-                                res.cookie("e", "7")
-                                res.redirect("/signup")
+                                res.cookie("e", "7");
+                                res.redirect("/signup");
                             }
                         } else {
-                            res.cookie("e", "6")
-                            res.redirect("/signup")
-                        } 
+                            res.cookie("e", "6");
+                            res.redirect("/signup");
+                        }
                     } else {
-                        res.cookie("e", "5")
-                        res.redirect("/signup")
+                        res.cookie("e", "5");
+                        res.redirect("/signup");
                     }
                 } else {
-                    res.cookie("e", "4")
-                    res.redirect("/signup")
+                    res.cookie("e", "4");
+                    res.redirect("/signup");
                 }
             } else {
-                res.cookie("e", "3")
-                res.redirect("/signup")
+                res.cookie("e", "3");
+                res.redirect("/signup");
             }
         } else {
-            res.cookie("e", "2")
-            res.redirect("/signup")
+            res.cookie("e", "2");
+            res.redirect("/signup");
         }
     } else {
-        res.cookie("e", "1")
-        res.redirect("/signup")
+        res.cookie("e", "1");
+        res.redirect("/signup");
     }
-})
+});
 app.post("/doinganotherthings", (req, res) => {
     let { username, password } = req.body;
-})
+});
 
 app.get("/login", (req, res) => {
-    res.render("login.ejs", {username: "", password: ""});
+    res.render("login.ejs", { username: "", password: "" });
 });
 
 app.get("/signup", (req, res) => {
-    res.cookie("e", "")
+    res.cookie("e", "");
     if (req.cookies.e == "1") {
-        res.render("signup.ejs", {name: "<p> Please don't leave this empty. ", username: "", password: "", confirmpassword: ""})
+        res.render("signup.ejs", {
+            name: "<p> Please don't leave this empty. ",
+            username: "",
+            password: "",
+            confirmpassword: ""
+        });
     } else if (req.cookies.e == "2") {
-        res.render("signup.ejs", {name: "", username: "<p> Please don't leave this empty. ", password: "", confirmpassword: ""})
+        res.render("signup.ejs", {
+            name: "",
+            username: "<p> Please don't leave this empty. ",
+            password: "",
+            confirmpassword: ""
+        });
     } else if (req.cookies.e == "3") {
-        res.render("signup.ejs", {name: "", username: "", password: "<p> Please don't leave this empty. </p>", confirmpassword: ""})
+        res.render("signup.ejs", {
+            name: "",
+            username: "",
+            password: "<p> Please don't leave this empty. </p>",
+            confirmpassword: ""
+        });
     } else if (req.cookies.e == "4") {
-        res.render("signup.ejs", {name: "", username: "", password: "", confirmpassword: "<p> Please don't leave this empty. </p>"})
+        res.render("signup.ejs", {
+            name: "",
+            username: "",
+            password: "",
+            confirmpassword: "<p> Please don't leave this empty. </p>"
+        });
     } else if (req.cookies.e == "5") {
-        res.render("signup.ejs", {name: "", username: "<p> Please use character between A to Z and 0 to 9 only. </p>", password: "", confirmpassword: ""})
+        res.render("signup.ejs", {
+            name: "",
+            username:
+                "<p> Please use character between A to Z and 0 to 9 only. </p>",
+            password: "",
+            confirmpassword: ""
+        });
     } else if (req.cookies.e == "6") {
-        res.render("signup.ejs", {name: "", username: "<p> This username cannot be used. </p>", password: "", confirmpassword: "nonono"})
+        res.render("signup.ejs", {
+            name: "",
+            username: "<p> This username cannot be used. </p>",
+            password: "",
+            confirmpassword: "nonono"
+        });
     } else if (req.cookies.e == "7") {
-        res.render("signup.ejs", {name: "", username: "", password: "", confirmpassword: "<p> This is not the same with password. </p>"} )
+        res.render("signup.ejs", {
+            name: "",
+            username: "",
+            password: "",
+            confirmpassword: "<p> This is not the same with password. </p>"
+        });
     } else {
-        res.render("signup.ejs", {name: "", username: "", password: "", confirmpassword: ""});
+        res.render("signup.ejs", {
+            name: "",
+            username: "",
+            password: "",
+            confirmpassword: ""
+        });
     }
 });
 
