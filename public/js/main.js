a3fd21f3b329329a189cfe076f4ea8b44da9bd18
barium-bromide <108572893+barium-bromide@users.noreply.github.com>
2023-02-10 11:18:09 +0800

    textbox unfinish

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,6 @@
 const choice1 = document.getElementById("choice-1");
 const choice2 = document.getElementById("choice-2");
+const screenHeight = screen.height;
 
 choice1.onclick = () => {
     choice1.classList.add("clicked");
@@ -10,3 +11,14 @@ choice2.onclick = () => {
     choice2.classList.add("clicked");
     choice1.classList.remove("clicked");
 };
+
+$('textarea').each(function () {
+  this.setAttribute('style', 'height:' + (this.scrollHeight / window.innerHeight * 100) + 'vh;overflow-y:scroll;');
+}).on('input', function () {
+  this.style.height = 'auto';
+  if (this.scrollHeight > window.innerHeight / 7) { // Maximum height of 50% of the viewport height
+    this.style.height = '14.2vh';
+  } else {
+    this.style.height = (this.scrollHeight / window.innerHeight * 100) + 'vh';
+  }
+});
