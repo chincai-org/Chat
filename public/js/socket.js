a8801dcaccbbc5879edf10d0d8016d081223b170
ddddddddrrdd3 <108572893+ddddddddrrdd3@users.noreply.github.com>
2023-02-15 14:52:11 +0800

    bug help to fix

--- public/js/socket.js
+++ public/js/socket.js
@@ -1,6 +1,24 @@
 const socket = io();
 
 socket.on("msg", (authorName, roomId, content, time) => {
+    const chat = document.getElementById("chatting")
+    if (currentRoom === roomId) {
+        console.log(authorName,content,time)
+        let container = document.createElement("div");
+        container.className = "container";
+        let name = document.createElement("h5");
+        name.innerText = authorName;
+        let msg = document.createElement("p");
+        msg.innerHTML = content;
+        container.appendChild(name);
+        container.appendChild(msg);
+        chat.appendChild(container);
+    }
+    //        <div class="container">
+    //<h5>Joe</h5>
+    //<p>Hello</p>
+    //<span class="time">31/12/2022 11:01</span>
+    //</div>
     // TODO append msg to html
     // time is unix timestamp convert urself ;)
 });
@@ -8,36 +26,47 @@ socket.on("msg", (authorName, roomId, content, time) => {
 socket.on("rooms", (rooms, pins) => {
     console.table(rooms);
     console.table(pins);
+
     const topics = document.getElementById("rooms")
-    let pinList = []
+    let pinList = [];
+
     if (!(pins.length === 0)) {
+
+        let textPin = document.createElement("p");
+        textPin.className = "text-pin";
+        textPin.innerText = "pin";
+        topics.appendChild(textPin);
+
         for (let pin = 0; pin < pins.length; pins++) {
+
             let topic = document.createElement("div");
-            topic.id = pins[pin]['_id']
+            topic.id = pins[pin]['_id'];
+
             let topicName = document.createElement("h5");
             topicName.title = "Right click for more info";
             topicName.innerText = pins[pin]['name'];
+
             topic.appendChild(topicName);
             topics.appendChild(topic);
             pinList.push(pins[pin]['name']);
-        }
-    }
+        };
+        textPin.innerText = "";
+        topics.appendChild(textPin);
+    };
     for (let room = 0; room < rooms.length - pins.length; room++) {
+
         if (!(rooms[room]['name'] in pinList)) {
+
             let topic = document.createElement("div");
             topic.id = rooms[room]['_id']
+
             let topicName = document.createElement("h5");
             topicName.title = "Right click for more info";
             topicName.innerText = rooms[room]['name'];
+            
             topic.appendChild(topicName);
             topics.appendChild(topic);
         }
 
     }
-    // TODO append room to rooms
-    // Object in rooms and pins looks something like this:
-    // {
-    //     _id: "xxx",
-    //     name: "xxx"
-    // }
 });
