f0fcbfe537884b8956538e6883b92f73fa588748
ddddddddrrdd3 <108572893+ddddddddrrdd3@users.noreply.github.com>
2023-02-15 12:31:18 +0800

    css stuff

--- public/js/socket.js
+++ public/js/socket.js
@@ -10,15 +10,17 @@ socket.on("rooms", (rooms, pins) => {
     console.table(pins);
     const topics = document.getElementById("rooms")
     let pinList = []
-    for (let pin = 0; pin < pins.length; pins++) {
-        let topic = document.createElement("div");
-        topic.id = pins[pin]['_id']
-        let topicName = document.createElement("h5");
-        topicName.title = "Right click for more info";
-        topicName.innerText = pins[pin]['name'];
-        topic.appendChild(topicName);
-        topics.appendChild(topic);
-        pinList.push(pins[pin]['name']);
+    if (!(pins.length === 0)) {
+        for (let pin = 0; pin < pins.length; pins++) {
+            let topic = document.createElement("div");
+            topic.id = pins[pin]['_id']
+            let topicName = document.createElement("h5");
+            topicName.title = "Right click for more info";
+            topicName.innerText = pins[pin]['name'];
+            topic.appendChild(topicName);
+            topics.appendChild(topic);
+            pinList.push(pins[pin]['name']);
+        }
     }
     for (let room = 0; room < rooms.length - pins.length; room++) {
         if (!(rooms[room]['name'] in pinList)) {
