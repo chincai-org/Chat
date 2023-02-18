eea2764cdb69d7de01346b36490f1e73a37976a5
Mortis66666 <mortisloverbs@gmail.com>
2023-02-18 13:53:01 +0800

    tidy up without fixing anything

--- public/js/main.js
+++ public/js/main.js
@@ -84,6 +84,86 @@ function clearMessage() {
     outerWrap.innerHTML = "";
 }
 
+function createTopic(room) {
+    let topic = document.createElement("div");
+    let contextMenu = document.getElementById(room._id);
+    topic.id = room._id;
+
+    topic.onclick = () => {
+        clearMessage();
+        textbox.classList.remove("hide");
+        currentRoom = room._id;
+        socket.emit("fetchmsg", cookieId, room._id);
+        contextMenu.classList.remove("active");
+    };
+
+    topic.oncontextmenu = e => {
+        e.preventDefault();
+        contextMenu.classList.add("active");
+        evt = evt || window.event;
+        let x = evt.clientX,
+            y = evt.clientY;
+        x =
+            x > window.innerWidth - contextMenu.offsetWidth
+                ? window.innerWidth - contextMenu.offsetWidth
+                : x;
+        y =
+            y > window.innerHeight - contextMenu.offsetHeight
+                ? window.innerHeight - contextMenu.offsetHeight
+                : y;
+        contextMenu.style.left = `${(x / window.innerHeight) * 100}vh`;
+        contextMenu.style.top = `${(y / window.innerHeight) * 100 - 0.3}vh`;
+        contextMenu.classList.add("active");
+    };
+
+    let topicName = document.createElement("h5");
+    topicName.title = "Right click for more info";
+    topicName.innerText = room.name;
+
+    let wrapper = document.createElement("div");
+    wrapper.className = "wrapper";
+
+    let menuContent = document.createElement("div");
+    menuContent.className = "menu-content";
+
+    let menu = document.createElement("ul");
+    menu.className = "menu";
+
+    let itemTrash = document.createElement("li");
+    itemTrash.className = "item";
+
+    let iTrash = document.createElement("i");
+    iTrash.className = "fa-solid fa-trash";
+    let spanTrash = document.createElement("span");
+    spanTrash.innerText = "Delete";
+
+    let copyId = document.createElement("div");
+    copyId.className = "copy-id";
+
+    let itemCopyId = document.createElement("li");
+    itemCopyId.className = "item";
+
+    let ICopyId = document.createElement("i");
+    ICopyId.className = "fa-solid fa-id-card-clip";
+    let spanCopyId = document.createElement("span");
+    spanCopyId.innerText = "Copy ID";
+
+    itemTrash.appendChild(iTrash);
+    itemTrash.appendChild(spanTrash);
+    menu.appendChild(itemTrash);
+    itemCopyId.appendChild(ICopyId);
+    itemCopyId.appendChild(spanCopyId);
+    copyId.appendChild(itemCopyId);
+    menuContent.appendChild(menu);
+    menuContent.appendChild(copyId);
+    wrapper.appendChild(menuContent);
+
+    topic.appendChild(wrapper);
+    topic.appendChild(topicName);
+
+    return topic;
+}
+
 function createMsg(id, authorName, content, time) {
     let date = new Date(time);
 
