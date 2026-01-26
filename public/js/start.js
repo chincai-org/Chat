5a377b9affa9799abe5982f5bdafe634a83be3db
barium-bromide <108572893+barium-bromide@users.noreply.github.com>
2026-01-26 18:27:26 +0800

    refactor: css styling for home page

--- public/js/start.js
+++
@@ -1,7 +0,0 @@
-const navigation = document.querySelector(".navigation");
-const navigationHeight = navigation.offsetHeight;
-
-document.documentElement.style.setProperty(
-    "--scroll-padding",
-    navigationHeight + "px",
-);
